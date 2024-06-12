902fe3cef3a1c7e16bff0eff66c3e889f4580154
guadasandoval <guadastoop@gmail.com>
2024-06-12 16:26:12 -0300

    bento practice

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: 
+      {backgroundImage: {
+        'hero-pattern': "url('./analog.jpg')",
+      }},
+    },
+    variants: {},
+    plugins: [],
+  }
